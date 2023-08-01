99793a1b1f5f992d396a9bb2a60c65746717eaec
PallabkumarS <pallabkumar26@gmail.com>
2023-08-02 03:08:14 +0600

    js practices

---
+++ Javscript/loop1.js
@@ -0,0 +1,25 @@
+// ajke amar mon valo nei loop
+// var String = "Ajke Mon Valo Nei";
+// for (var i = 0; i < 39; i++) {
+//   console.log([i]);
+//   console.log(String);
+// }
+// display number loop
+// for (var i = 0; i < 99; i++) {
+//   if (56 < i) {
+//     console.log([i]);
+//   }
+//   continue;
+// }
+// even or odd Number
+// for (var i = 412; i < 556; i++) {
+//   if (i % 2 == 0) {
+//     console.log([i]);
+//   }
+// }
+for (var i = 30; i < 86; i++) {
+  if (i > 44) {
+    break;
+  }
+  console.log([i]);
+}
